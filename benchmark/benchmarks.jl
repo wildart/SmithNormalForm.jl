35f7044763e54211124f20babbb6569917f8af72
wildart <wildart@gmail.com>
2018-04-09 20:13:30 -0400

    benchmark update

--- benchmark/benchmarks.jl
+++ benchmark/benchmarks.jl
@@ -2,7 +2,7 @@ using SmithNormalForm
 using BenchmarkTools
 
 srand(2903872398473)
-rows, cols = 7, 13
+rows, cols = 64, 93
 TMP = rand(1:10, rows*cols)
 D = reshape(TMP, rows, cols)
 i, j = 2, 3
@@ -11,18 +11,21 @@ a, b, c, d = 1, 2, 3, 4
 suite = BenchmarkGroup()
 
 suite["internal"] = BenchmarkGroup(["swap", "elimination"])
-suite["snf"] = BenchmarkGroup(["decomposition"])
+# suite["snf"] = BenchmarkGroup(["decomposition"])
 
-suite["internal"]["col_swap"] = @benchmarkable SmithNormalForm.cswap!(D, i, j) samples=100000
-suite["internal"]["row_swap"] = @benchmarkable SmithNormalForm.rswap!(D, i, j) samples=100000
+suite["internal"]["col_swap"] = @benchmarkable SmithNormalForm.cswap!(D, i, j) gctrial=true evals=1000 samples=100000
+suite["internal"]["row_swap"] = @benchmarkable SmithNormalForm.rswap!(D, i, j) gctrial=true evals=1000 samples=100000
 
-suite["internal"]["col_elim"] = @benchmarkable SmithNormalForm.colelimination(D, a, b, c, d, i, j)
-suite["internal"]["row_elim"] = @benchmarkable SmithNormalForm.rowelimination(D, a, b, c, d, i, j)
+suite["internal"]["col_elim"] = @benchmarkable SmithNormalForm.colelimination(D, a, b, c, d, i, j) evals=500 gctrial=true
+suite["internal"]["row_elim"] = @benchmarkable SmithNormalForm.rowelimination(D, a, b, c, d, i, j) evals=500 gctrial=true
 
-Z = rand(1:100, 100, 120)
+Z = rand(1:100, 500, 520)
+Z[Z .> 66] = 1
+Z[Z .> 33] = -1
+Z[Z .> 0] = 0
 suite["snf"]["dense"] = @benchmarkable snf(Z)
 
-Z = sprand(Int, 100, 120, 0.3)
+Z = sprand(Int, 500, 520, 0.3)
 Z[Z .< 0] = -1
 Z[Z .> 0] = 1
 suite["snf"]["sparse"] = @benchmarkable snf(Z)
