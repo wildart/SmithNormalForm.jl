d40a3b8c8b380cdcf2db2a3ca36645eddae4347c
Thomas Christensen <tchr@mit.edu>
2021-03-09 09:41:49 -0500

    Avoid abstractly typed fields in `Smith`; also export `Smith`
    - add doc-string to `smith`

--- src/SmithNormalForm.jl
+++ src/SmithNormalForm.jl
@@ -8,27 +8,44 @@ using Base.CoreLogging
 import Base: show
 import LinearAlgebra: diagm
 
-export snf, smith
+export snf, smith, Smith
 
 include("bezout.jl")
 include("snf.jl")
 
-struct Smith{P,Q<:AbstractMatrix} <: Factorization{P}
+
+struct Smith{P,Q<:AbstractMatrix{P},V<:AbstractVector{P}} <: Factorization{P}
     S::Q
     Sinv::Q
     T::Q
     Tinv::Q
-    SNF::AbstractVector{P}
-    Smith{P,Q}(S::AbstractMatrix{P}, Sinv::AbstractMatrix{P},
-               T::AbstractMatrix{P}, Tinv::AbstractMatrix{P},
-               A::AbstractVector{P}) where {P,Q} = new(S, Sinv, T, Tinv, A)
+    SNF::V
+    Smith{P,Q,V}(S::AbstractMatrix{P}, Sinv::AbstractMatrix{P},
+                 T::AbstractMatrix{P}, Tinv::AbstractMatrix{P},
+                 D::AbstractVector{P}) where {P,Q,V} = new(S, Sinv, T, Tinv, D)
 end
-Smith(S::AbstractMatrix{P}, T::AbstractMatrix{P}, A::AbstractVector{P}) where {P} =
-    Smith{P,typeof(A)}(S, similar(S, 0, 0), T, similar(T, 0, 0), A)
+Smith(S::AbstractMatrix{P}, T::AbstractMatrix{P}, SNF::AbstractVector{P}) where {P} =
+    Smith{P,typeof(S),typeof(SNF)}(S, similar(S, 0, 0), T, similar(T, 0, 0), SNF)
 
-function smith(X::AbstractMatrix{P}; inverse=true) where {P}
-    S, T, A, Sinv, Tinv = snf(X, inverse=inverse)
-    return Smith{P, typeof(X)}(S, Sinv, T, Tinv, diag(A))
+"""
+    smith(X::AbstractMatrix{P}; inverse::Bool=true) --> Smith{P,Q,V}
+
+Return a Smith normal form of an integer matrix `X` as a `Smith` structure (of element type
+`P`, matrix type `Q`, and invariant factor type `V`).
+
+The Smith normal form is well-defined for any matrix ``m×n`` matrix `X` with elements in a
+principal domain (PID; e.g., integers) and provides a decomposition of `X` into ``m×m``,
+``m×n``, and `S`, `Λ`, and `T` as `X = SΛT`, where `Λ` is a diagonal matrix with entries 
+("invariant factors") Λᵢ ≥ Λᵢ₊₁ ≥ 0 with nonzero entries divisible in the sense Λᵢ | Λᵢ₊₁.
+The invariant factors can be obtained from [`diag(::Smith)`](@ref).
+
+`S` and `T` are invertible matrices; if the keyword argument `inverse` is true (default),
+the inverse matrices are computed and returned as part of the `Smith` factorization.
+"""
+function smith(X::AbstractMatrix{P}; inverse::Bool=true) where {P}
+    S, T, D, Sinv, Tinv = snf(X, inverse=inverse)
+    SNF = diag(D)
+    return Smith{P, typeof(X), typeof(SNF)}(S, Sinv, T, Tinv, SNF)
 end
 
 """Retrive SNF matrix from the factorization"""
